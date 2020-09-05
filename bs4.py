854146111ab7e6b1f4c2a73574cf966813e1fb61
skarnata <setiawankarnatachan@hotmail.com>
2020-09-05 19:54:19 +0700

    Penambahan file bs4 dan use_request

--- bs4.py
+++ bs4.py
@@ -1,11 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://detik.com"
 try:
     response = requests.get(url)
     if response.status_code == 200:
         print(f"Success ! Response = {response.status_code}")
-        print(f"Content : {response.text}")
+        # print(f"Content : {response.text}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        print(soup.title.string)
     else:
         print(f"Wooops, ada kesalahan requests {response.status_code}")
 except Exception as e:
